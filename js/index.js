9a468e29149344e4e7ccdc0adaf091df6fa4b51c
MariaSvidryk <m.svidrik@gmail.com>
2020-06-07 13:59:24 +0000

    Added JS

---
+++ js/index.js
@@ -0,0 +1,103 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+
+const currentDate = new Date().getFullYear();
+
+const concertTime = new Date(`June 25 ${currentDate} 00:00:00`);
+
+// Update countdown time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = concertTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+// Run every second
+setInterval(updateCountdown, 1000);
+
+//MIDDLE PART
+
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const daySelect = document.getElementById('day');
+
+populateUI();
+
+let ticketPrice = +daySelect.value;
+
+// Save selected day index and price
+function setDayData(dayIndex, dayPrice) {
+  localStorage.setItem('selectedDayIndex', dayIndex);
+  localStorage.setItem('selectedDayPrice', dayPrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedDayIndex = localStorage.getItem('selectedDayIndex');
+
+  if (selectedDayIndex !== null) {
+    daySelect.selectedIndex = selectedDayIndex;
+  }
+}
+
+// Day select event
+daySelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setDayData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
+updateSelectedCount();
