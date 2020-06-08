568e0ce5bc19d2f697bdd27e95318655b538cc3a
MariaSvidryk <m.svidrik@gmail.com>
2020-06-08 12:53:03 +0000

    Change Formatting

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const countdown = document.getElementById('countdown');
-const year = document.getElementById('year');
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
 
 const currentDate = new Date().getFullYear();
 
@@ -21,9 +21,9 @@ function updateCountdown() {
 
   // Add values to DOM
   days.innerHTML = d;
-  hours.innerHTML = h < 10 ? '0' + h : h;
-  minutes.innerHTML = m < 10 ? '0' + m : m;
-  seconds.innerHTML = s < 10 ? '0' + s : s;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
 }
 
 // Run every second
@@ -31,13 +31,13 @@ setInterval(updateCountdown, 1000);
 
 //MIDDLE PART
 
-const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-const daySelect = document.getElementById('day');
-const reset = document.getElementById('reset');
-const buy = document.getElementById('buy')
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row.seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const daySelect = document.getElementById("day");
+const reset = document.getElementById("reset");
+const buy = document.getElementById("buy");
 
 populateUI();
 
@@ -45,17 +45,17 @@ let ticketPrice = +daySelect.value;
 
 // Save selected day index and price
 function setDayData(dayIndex, dayPrice) {
-  localStorage.setItem('selectedDayIndex', dayIndex);
-  localStorage.setItem('selectedDayPrice', dayPrice);
+  localStorage.setItem("selectedDayIndex", dayIndex);
+  localStorage.setItem("selectedDayPrice", dayPrice);
 }
 
 // Update total and count
 function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
-  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -65,17 +65,17 @@ function updateSelectedCount() {
 
 // Get data from localstorage and populate UI
 function populateUI() {
-  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
 
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
-        seat.classList.add('selected');
+        seat.classList.add("selected");
       }
     });
   }
 
-  const selectedDayIndex = localStorage.getItem('selectedDayIndex');
+  const selectedDayIndex = localStorage.getItem("selectedDayIndex");
 
   if (selectedDayIndex !== null) {
     daySelect.selectedIndex = selectedDayIndex;
@@ -83,19 +83,19 @@ function populateUI() {
 }
 
 // Day select event
-daySelect.addEventListener('change', e => {
+daySelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
   setDayData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
 // Seat click event
-container.addEventListener('click', e => {
+container.addEventListener("click", (e) => {
   if (
-    e.target.classList.contains('seat') &&
-    !e.target.classList.contains('occupied')
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
   ) {
-    e.target.classList.toggle('selected');
+    e.target.classList.toggle("selected");
 
     updateSelectedCount();
   }
@@ -104,24 +104,24 @@ container.addEventListener('click', e => {
 // Initial count and total set
 updateSelectedCount();
 
-const toggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-const modal = document.getElementById('modal');
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
 
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('show-nav');
-  document.body.addEventListener('click', closeNavbar);
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("show-nav");
+  document.body.addEventListener("click", closeNavbar);
 });
 
 // Show modal
-open.addEventListener('click', () => modal.classList.add('show-modal'));
+open.addEventListener("click", () => modal.classList.add("show-modal"));
 
 // Hide modal
-close.addEventListener('click', () => modal.classList.remove('show-modal'));
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
 
 // Hide modal on outside click
-window.addEventListener('click', e =>
-  e.target == modal ? modal.classList.remove('show-modal') : false
+window.addEventListener("click", (e) =>
+  e.target == modal ? modal.classList.remove("show-modal") : false
 );
 
